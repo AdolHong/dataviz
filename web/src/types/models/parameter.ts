b6d40d7c977a5ad2a7c5caf6e4e6396f516e22b1
adol hong <adol.hong@gmail.com>
2025-03-29 11:12:19 +0800

    v1 update

--- web/src/types/models/parameter.ts
+++ web/src/types/models/parameter.ts
@@ -1,17 +1,45 @@
+export interface SingleSelectParamConfig {
+  Config: 'single_select';
+  choices: string[];
+  default: string;
+}
+
+export interface MultiSelectParamConfig {
+  Config: 'multi_select';
+  choices: string[];
+  default: string[];
+  sep: string;
+  wrapper: string;
+}
+
+export interface DatePickerParamConfig {
+  Config: 'date_picker';
+  dateFormat: string;
+  default: string;
+}
+
+export interface MultiInputParamConfig {
+  Config: 'multi_input';
+  default: string[];
+  sep: string;
+  wrapper: string;
+}
+
+export interface SingleInputParamConfig {
+  Config: 'single_input';
+  default: string;
+}
+
 // 参数接口
 export interface Parameter {
   id: string; // 自动生成
   name: string;
   alias?: string; // 参数可以有别名, 一般是中文，便于理解参数
   description?: string;
-  type: string;
-  default?: string;
-  choices?: string[];
-  format?: {
-    dateFormat?: string;
-    timeFormat?: string;
-    datetimeFormat?: string;
-    sep?: string;
-    wrapper?: string;
-  };
+  paramConfig:
+    | SingleSelectParamConfig
+    | MultiSelectParamConfig
+    | DatePickerParamConfig
+    | MultiInputParamConfig
+    | SingleInputParamConfig;
 }

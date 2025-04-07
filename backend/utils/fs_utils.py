380bbcc8a0a3f906282d61ba84add63d8a06c517
adol hong <adol.hong@gmail.com>
2025-04-07 09:54:13 +0800

    v1 update

--- backend/utils/fs_utils.py
+++ backend/utils/fs_utils.py
@@ -7,12 +7,13 @@ from models.fs_models import FileSystemItem, FileSystemItemType
 # 数据文件路径
 DATA_DIR = Path(__file__).parent.parent.parent / "data"
 FS_DATA_FILE = os.path.join(DATA_DIR, "fs_items.json")
-TOKEN_FILE = os.path.join(DATA_DIR, "token.duckdb")
+
 
 # 设置实际文件存储的基础路径
 FILE_STORAGE_PATH = os.path.join(DATA_DIR, "files")
 FILE_DELETED_PATH = os.path.join(DATA_DIR, "files_deleted")
 FILE_CACHE_PATH = os.path.join(DATA_DIR, "files_cache")
+FILE_TOKEN_PATH = os.path.join(DATA_DIR, "tokens")
 
 
 

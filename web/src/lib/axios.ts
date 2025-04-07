b529101a5053f885c9332cea323d0b196be9d65f
adol hong <adol.hong@gmail.com>
2025-04-07 14:40:34 +0800

    v1 update

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 // 个人mac
-const BASE_URL = 'http://localhost:8000/api';
+// const BASE_URL = 'http://localhost:8000/api';
+
+// 公司服务器
+const BASE_URL = 'http://10.128.252.212:8080/api';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
